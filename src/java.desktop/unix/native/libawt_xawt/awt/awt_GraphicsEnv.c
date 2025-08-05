de89e612dc96a28f6d93164c6c4fe725c17a7f28
Goetz Lindenmaier <Goetz.Lindenmaier@sap.com>
2025-08-05 13:36:18 +0200

    backport 18190519e73705281adf3f94d710d000e75b1729

--- src/java.desktop/unix/native/libawt_xawt/awt/awt_GraphicsEnv.c
+++ src/java.desktop/unix/native/libawt_xawt/awt/awt_GraphicsEnv.c
@@ -1750,7 +1750,7 @@ Java_sun_awt_X11GraphicsDevice_initXrandrExtension
 // ---------------------------------------------------
 // display mode change via XRRSetCrtcConfig
 // ---------------------------------------------------
-
+#if !defined(NO_XRANDR)
 static jint refreshRateFromModeInfo(const XRRModeInfo *modeInfo) {
     if (!modeInfo->hTotal || !modeInfo->vTotal) {
         return 0;
@@ -2034,6 +2034,7 @@ static void xrrChangeDisplayMode(jint screen, jint width, jint height, jint refr
         }
         awt_XRRFreeScreenResources(res);
 }
+#endif
 
 // ---------------------------------------------------
 // display mode change via XRRSetCrtcConfig
