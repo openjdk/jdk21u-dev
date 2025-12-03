da02d69d6fa8ee4cf145cdc0c430457802c5e9a4
Goetz Lindenmaier <Goetz.Lindenmaier@sap.com>
2025-12-03 17:24:24 +0100

    resolves

--- src/java.xml/share/classes/jdk/xml/internal/Utils.java
+++ src/java.xml/share/classes/jdk/xml/internal/Utils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,7 +25,10 @@
 
 package jdk.xml.internal;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 /**
  * General utility. Use JdkXmlUtils for XML processing related functions.
