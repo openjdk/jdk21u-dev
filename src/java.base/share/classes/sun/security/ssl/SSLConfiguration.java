168f6716a4aecdddf71994a8b4ab62d26f8c94b8
Goetz Lindenmaier <Goetz.Lindenmaier@sap.com>
2026-01-18 10:02:34 +0100

    fix build - hunk placed wrong

--- src/java.base/share/classes/sun/security/ssl/SSLConfiguration.java
+++ src/java.base/share/classes/sun/security/ssl/SSLConfiguration.java
@@ -136,6 +136,33 @@ final class SSLConfiguration implements Cloneable {
             }
         }
         useExtendedMasterSecret = supportExtendedMasterSecret;
+
+        /*
+         * jdk.tls.server.newSessionTicketCount system property
+         * Sets the number of NewSessionTickets sent to a TLS 1.3 resumption
+         * client.  The value must be between 0 and 10.  Default is defined by
+         * SERVER_NST_DEFAULT.
+         */
+        Integer nstServerCount = GetIntegerAction.privilegedGetProperty(
+            "jdk.tls.server.newSessionTicketCount");
+        if (nstServerCount == null || nstServerCount < 0 ||
+            nstServerCount > 10) {
+            serverNewSessionTicketCount = SERVER_NST_DEFAULT;
+            if (nstServerCount != null && SSLLogger.isOn &&
+                SSLLogger.isOn("ssl,handshake")) {
+                SSLLogger.fine(
+                    "jdk.tls.server.newSessionTicketCount defaults to " +
+                        SERVER_NST_DEFAULT + " as the property was not " +
+                        "between 0 and 10");
+            }
+        } else {
+            serverNewSessionTicketCount = nstServerCount;
+            if (SSLLogger.isOn && SSLLogger.isOn("ssl,handshake")) {
+                SSLLogger.fine(
+                    "jdk.tls.server.newSessionTicketCount set to " +
+                        serverNewSessionTicketCount);
+            }
+        }
     }
 
     SSLConfiguration(SSLContextImpl sslContext, boolean isClientMode) {
@@ -322,33 +349,6 @@ final class SSLConfiguration implements Cloneable {
 
         if (handshakeListeners.isEmpty()) {
             handshakeListeners = null;
-        }
-
-        /*
-         * jdk.tls.server.newSessionTicketCount system property
-         * Sets the number of NewSessionTickets sent to a TLS 1.3 resumption
-         * client.  The value must be between 0 and 10.  Default is defined by
-         * SERVER_NST_DEFAULT.
-         */
-        Integer nstServerCount = GetIntegerAction.privilegedGetProperty(
-            "jdk.tls.server.newSessionTicketCount");
-        if (nstServerCount == null || nstServerCount < 0 ||
-            nstServerCount > 10) {
-            serverNewSessionTicketCount = SERVER_NST_DEFAULT;
-            if (nstServerCount != null && SSLLogger.isOn &&
-                SSLLogger.isOn("ssl,handshake")) {
-                SSLLogger.fine(
-                    "jdk.tls.server.newSessionTicketCount defaults to " +
-                        SERVER_NST_DEFAULT + " as the property was not " +
-                        "between 0 and 10");
-            }
-        } else {
-            serverNewSessionTicketCount = nstServerCount;
-            if (SSLLogger.isOn && SSLLogger.isOn("ssl,handshake")) {
-                SSLLogger.fine(
-                    "jdk.tls.server.newSessionTicketCount set to " +
-                        serverNewSessionTicketCount);
-            }
         }
     }
 
